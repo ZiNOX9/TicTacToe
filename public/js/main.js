1b0a5d11ae8c1b176c92933728abd3a496b8abc3
ZiNOX <arjunporwal479@gmail.com>
2018-06-13 21:40:02 +0530

    Adding prompt

--- public/js/main.js
+++ public/js/main.js
@@ -10,7 +10,6 @@ window.addEventListener('beforeinstallprompt', (e) => {
   e.preventDefault();
   // Stash the event so it can be triggered later.
   deferredPrompt = e;
-});
   // Update UI notify the user they can add to home screen
   btnAdd.style.display = 'block';
 });
