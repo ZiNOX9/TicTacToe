26a3ff522cb43e135b43a1640c2ef1fb93dd114f
Arjun Porwal <arjunporwal479@gmail.com>
2018-09-01 16:33:47 +0530

    Service Worker changed

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,13 +1,7 @@
-/**
- * Cache version, change name to force reload
- */
-var CACHE_VERSION = 'v1';
+//This is the service worker with the Cache-first network
 
-
-/**
- * Stuff to put in the cache at install
- */
-var CACHE_FILES  = [
+var CACHE = 'pwabuilder-precache';
+var precacheFiles = [
     './',
     '/',
     'index.html',
@@ -20,48 +14,126 @@ var CACHE_FILES  = [
     'css/style.css',
 ];
 
-
-/**
- * Service worker 'install' event.
- * If all the files are successfully cached, then the service worker will be installed.
- * If any of the files fail to download, then the install step will fail.
- */
-this.addEventListener('install', function(event) {
-   event.waitUntil(
-        caches.open(CACHE_VERSION).then(function(cache) {
-            console.log('Installing...');
-            return cache.addAll(CACHE_FILES);
-        }).catch(function(a) {
-            console.log(a);
-        })
-    ); // waitUntil
+//Install stage sets up the cache-array to configure pre-cache content
+self.addEventListener('install', function (evt) {
+    console.log('[PWA Builder] The service worker is being installed.');
+    evt.waitUntil(precache().then(function () {
+        console.log('[PWA Builder] Skip waiting on install');
+        return self.skipWaiting();
+    }));
 });
 
 
-/**
- * After a service worker is installed and the user navigates to a different page or refreshes,
- * the service worker will begin to receive fetch events.
- *
- * Network-first approach: if online, request is fetched from network and not from cache
- */
-this.addEventListener('fetch', function(event) {
-    event.respondWith(function() {
+//allow sw to control of current page
+self.addEventListener('activate', function (event) {
+    console.log('[PWA Builder] Claiming clients for current page');
+    return self.clients.claim();
+});
+
+self.addEventListener('fetch', function (evt) {
+    console.log('[PWA Builder] The service worker is serving the asset.' + evt.request.url);
+    evt.respondWith(fromCache(evt.request).catch(fromServer(evt.request)));
+    evt.waitUntil(update(evt.request));
+});
+
+
+function precache() {
+    return caches.open(CACHE).then(function (cache) {
+        return cache.addAll(precacheFiles);
+    });
+}
+
+function fromCache(request) {
+    //we pull files from the cache first thing so we can show them fast
+    return caches.open(CACHE).then(function (cache) {
+        return cache.match(request).then(function (matching) {
+            return matching || Promise.reject('no-match');
+        });
+    });
+}
+
+function update(request) {
+    //this is where we call the server to get the newest version of the 
+    //file to use the next time we show view
+    return caches.open(CACHE).then(function (cache) {
+        return fetch(request).then(function (response) {
+            return cache.put(request, response);
+        });
+    });
+}
+
+function fromServer(request) {
+    //this is the fallback if it is not in the cache to go to the server and get it
+    return fetch(request).then(function (response) { return response });
+}
+
+
+
+
+// /**
+//  * Cache version, change name to force reload
+//  */
+// var CACHE_VERSION = 'v1';
+
+
+// /**
+//  * Stuff to put in the cache at install
+//  */
+// var CACHE_FILES  = [
+//     './',
+//     '/',
+//     'index.html',
+//     'js/bootstrap.min.js',
+//     'js/jquery.min.js',
+//     'js/main.js',
+//     'js/tether.min.js',
+//     'css/bootstrap.css',
+//     'css/font-awesome.min.css',
+//     'css/style.css',
+// ];
+
+
+// /**
+//  * Service worker 'install' event.
+//  * If all the files are successfully cached, then the service worker will be installed.
+//  * If any of the files fail to download, then the install step will fail.
+//  */
+// this.addEventListener('install', function(event) {
+//    event.waitUntil(
+//         caches.open(CACHE_VERSION).then(function(cache) {
+//             console.log('Installing...');
+//             return cache.addAll(CACHE_FILES);
+//         }).catch(function(a) {
+//             console.log(a);
+//         })
+//     ); // waitUntil
+// });
+
+
+// /**
+//  * After a service worker is installed and the user navigates to a different page or refreshes,
+//  * the service worker will begin to receive fetch events.
+//  *
+//  * Network-first approach: if online, request is fetched from network and not from cache
+//  */
+// this.addEventListener('fetch', function(event) {
+//     event.respondWith(function() {
         
-        var res = returnFromServer(event);
-        if (res) {return res;}
+//         var res = returnFromServer(event);
+//         if (res) {return res;}
 
-        caches.match(event.request).then(function(res){
-            // Cache hit - return response
-            if(res){
-                return res;
-            }
+//         caches.match(event.request).then(function(res){
+//             // Cache hit - return response
+//             if(res){
+//                 return res;
+//             }
 
-            // no response
-            return null;
-        })
+//             // no response
+//             return null;
+//         })
 
-    }());
-});
+//     }());
+// });
 
 
 /**
